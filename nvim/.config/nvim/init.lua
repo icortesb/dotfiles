a92e23f514f7d67d9e214d03451c362306e9f57c
Ivan Cortes <ivan@mediadigitalgroup.com>
2026-02-23 09:39:12 -0300

    feat: add init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,13 @@
-require('config.options')
-require('config.keybinds')
-require('config.lazy')
+require("config.options")
+require("config.keybinds")
+require("config.lazy")
+vim.env.PATH = vim.env.PATH .. ":/home/icortesb/.config/composer/vendor/bin"
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        local dir = vim.fn.expand("%:p:h")
+        if vim.fn.isdirectory(dir) == 1 then
+            vim.cmd("lcd " .. dir)
+        end
+    end,
+})
